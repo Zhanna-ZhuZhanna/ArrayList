3b85fdddda3a0db97d45c708c10fe61cbb96a493
Zhanna <yellowcucumber27@gmail.com>
2020-11-07 22:20:58 +0300

    Create PrintList and AddToTheBeginning methods

--- ConsoleAppForDebugging/Program.cs
+++ ConsoleAppForDebugging/Program.cs
@@ -9,7 +9,10 @@ namespace ConsoleAppForDebugging
             ArrayList list = new ArrayList();
             Console.WriteLine("The length of list is " + list.Length);
             list.Add(100);
+            list.PrintTheList();
             Console.WriteLine("The length of list is " + list.Length);
+            list.AddElementToTheBeginning(50);
+            list.PrintTheList();
         }
     }
 }
--- DataStructures/ArrayList.cs
+++ DataStructures/ArrayList.cs
@@ -16,9 +16,6 @@ namespace DataStructures
             Length = 0;
         }
 
-
-
-
         public void Add(int value)
         {
             if (Length>=_array.Length)
@@ -28,7 +25,40 @@ namespace DataStructures
             _array[Length] = value;
             Length++;
         }
+        public void ReverseTheList()
+        {
+            int[] newArray = new int[_array.Length];
+            for (int i=0;i<Length;i++)
+            {
+                newArray[i] = _array[Length - 1 - i];
+            }
+            Array.Copy(newArray, _array, _array.Length);
+        }
+        public void AddElementToTheBeginning(int value)
+        {
+            if (Length>=_array.Length)
+            {
+                RiseSize();
+            }
+            int[] newArray = new int[_array.Length+1];
+            newArray[0] = value;
 
+            Array.Copy(_array,0,newArray,1,_array.Length);
+            _array = new int[newArray.Length];
+            Array.Copy(newArray, _array, newArray.Length);
+            Length++;
+
+        }
+
+        public void PrintTheList()
+        {
+            string n = "";
+            for(int i=0;i<Length;i++)
+            {
+                n += _array[i] + " ";
+            }
+            Console.WriteLine(n);
+        }
         public override bool Equals(object obj)         //для тестов
         {
             return true;
