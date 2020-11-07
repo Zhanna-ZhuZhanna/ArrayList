3785fe6cdd553e4301a6dfdf1a234ad36173b0be
Zhanna <yellowcucumber27@gmail.com>
2020-11-07 21:36:13 +0300

    Solving problems with the solution

---
+++ ConsoleAppForDebugging/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DataStructures;
+namespace ConsoleAppForDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            Console.WriteLine("The length of list is " + list.Length);
+            list.Add(100);
+            Console.WriteLine("The length of list is " + list.Length);
+        }
+    }
+}
--- ConsoleProjectForDebugging/Program.cs
+++ ConsoleProjectForDebugging/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleProjectForDebugging
     {
         static void Main(string[] args)
         {
-            
+            ArrayList list =new ArrayList();
+            Console.WriteLine("The length of list is "+list.Length);
+            list.Add(100);
+            Console.WriteLine("The length of list is "+list.Length);
+            Console.WriteLine(list.ToString);
         }
     }
 }
